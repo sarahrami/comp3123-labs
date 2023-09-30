7426b9f2ee7bef13f69ce337b0c6798639974530
sarahrami <sarahrami97@gmail.com>
2023-09-29 22:03:22 -0400

    update

---
+++ 101376641_COMP3123-exec04/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const SERVER_PORT = process.env | 8089;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/hello", (req, res) => {
+    res.send('Hello Express Js')
+})
+
+// query parameter
+app.get("/user", (req, res) => {
+    data = req.query
+    res.send(data)
+})
+//path parameter
+app.post("/user/:fname/:lname", (req, res) => {
+    data = req.params
+    let firstName = req.params.fname
+    let lastName = req.params.lname
+    res.send(data)
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log('Server is running');
+  });
+  
